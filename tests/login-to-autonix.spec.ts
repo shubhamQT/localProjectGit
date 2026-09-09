e3ceca633208978fdd8b5b1bbdcfde74ee81fadf
Shubham Bhele <shubham.bhele@qtsolv.com>
2026-09-09 12:03:21 +0530

    chore: sync test framework [2026-09-09T06:33:21.170Z]

--- tests/login-to-autonix.spec.ts
+++ tests/login-to-autonix.spec.ts
@@ -2,9 +2,8 @@ import { test, expect } from '@support/fixtures';
 import env from '@support/env';
 
 test('Login as Project Owner', { tag: ["@P0","@case-662347a8-27a4-4508-bca2-8a1f0eedacd7","@req-9c04f31a-cd93-4ae7-877d-87171e935912"] }, async ({ page, autonixPage, loginPage, dashboardPage }) => {
-  const vars: Record<string, string> = {};
   await test.step('Navigate to URL — url', async () => {
-    await page.goto('/{{env.baseURL}}');
+    await page.goto(env.baseURL);
   });
   await test.step('Assert visible — login', async () => {
     await autonixPage.expectSignInVisible();
@@ -13,13 +12,13 @@ test('Login as Project Owner', { tag: ["@P0","@case-662347a8-27a4-4508-bca2-8a1f
     await autonixPage.clickSignIn();
   });
   await test.step('Assert visible — email', async () => {
-    await loginPage.expectSignInVisible();
+    await loginPage.expectLoginEmailVisible();
   });
   await test.step('Click — email', async () => {
-    await loginPage.fillLoginEmail(vars["env.username"]);
+    await loginPage.fillLoginEmail(env.username);
   });
   await test.step('Fill — password', async () => {
-    await loginPage.fillLoginPassword(vars["env.password"]);
+    await loginPage.fillLoginPassword(env.password);
   });
   await test.step('Click — signin button', async () => {
     await loginPage.clickLoginSubmit();
